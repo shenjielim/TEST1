f19aaa8583fb32fb226197b440d713bc79a49c33
limshenjie <limshenjie@hotmail.com>
2023-02-01 23:47:01 +0800

    Answer added for jeff

--- app/app.py
+++ app/app.py
@@ -1,12 +1,72 @@
 import os
-import re
 
 
 # Write your answer here
 def calculate_revenue(items_list, number_of_customers):
     # Modify code below
-    return 1
+    mergeSort(items_list)
+    max_quantity = items_list[0]
+    revenue = 0
+    current_index = 0
+    for customer_num in range(number_of_customers):
+        current_item = items_list[current_index]
+        if current_item < max_quantity:
+            current_index = 0
+            current_item = items_list[current_index]
+            max_quantity = current_item
+        revenue += current_item
+
+        # Post purchase
+        items_list[current_index] = current_item - 1
+        current_index += 1
+
+    return revenue
     # Code ends here
+
+
+# Python program for implementation of MergeSort
+def mergeSort(arr):
+    if len(arr) > 1:
+
+        # Finding the mid of the array
+        mid = len(arr)//2
+
+        # Dividing the array elements
+        L = arr[:mid]
+
+        # into 2 halves
+        R = arr[mid:]
+
+        # Sorting the first half
+        mergeSort(L)
+
+        # Sorting the second half
+        mergeSort(R)
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] >= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
 
 
 class app:
@@ -23,8 +83,9 @@ class app:
                 items_list = [int(x) for x in lines[0].strip().split(",")]
                 number_of_customers = int(lines[1])
                 print("Executing " + test_folder)
+                print(f"List of items start: {items_list}")
                 result = calculate_revenue(items_list, number_of_customers)
-                print(f"List of items: {items_list}")
+                print(f"List of items end: {items_list}")
                 print("Number of customers: " +  str(number_of_customers))
                 print("Returned result: " + str(result))
                 print()
