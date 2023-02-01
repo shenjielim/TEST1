f0fecfb47e6600bdc7cc88c3de6715119505a39b
limshenjie <limshenjie@hotmail.com>
2023-02-01 23:47:01 +0800

    Answer added for jeff

--- app/app.py
+++ app/app.py
@@ -78,20 +78,17 @@ class app:
         # For loop each test case and read contents
         for test_folder in os.listdir(os.curdir):
             if os.path.isdir(test_folder) and test_folder.startswith("test_"):
-                test_path = test_folder + "/"
-                f = open(test_folder + "/" + os.listdir(test_path)[0], "r")
+                test_path = test_folder + "/" + test_folder + ".txt"
+                f = open(test_path, "r")
                 lines = f.readlines()
                 items_list = [int(x) for x in lines[0].strip().split(",")]
                 number_of_customers = int(lines[1])
                 print("Executing " + test_folder)
                 print(f"List of items start: {items_list}")
-                tic = time.perf_counter()
                 result = calculate_revenue(items_list, number_of_customers)
-                toc = time.perf_counter()
                 print(f"List of items end: {items_list}")
-                print("Number of customers: " + str("{:,}".format(number_of_customers)))
-                print("Returned result: " + str("{:,}".format(result)))
-                print(f'Time taken: {toc - tic:0.4f}s')
+                print("Number of customers: " +  str(number_of_customers))
+                print("Returned result: " + str(result))
                 print()
                 f.close()
         # Pass contents as arguments for test functions
