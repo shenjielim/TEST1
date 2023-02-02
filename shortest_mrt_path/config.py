53281e8dc55ce0e0c2a50e2f04101583f75b32b5
limshenjie <limshenjie@hotmail.com>
2023-02-02 23:21:30 +0800

    Added mrt test case 3

--- shortest_mrt_path/config.py
+++ shortest_mrt_path/config.py
@@ -9,8 +9,14 @@ start_mrt_1 = "Khatib"
 end_mrt_1 = "Ang Mo Kio"
 mrt_lines_1 = {"Red": lines["Red"]}
 
-# Test Case 1 Params
+# Test Case 2 Params
 test_name_2 = "Test case 2"
 start_mrt_2 = "Marina Bay"
 end_mrt_2 = "Dhoby Ghaut"
 mrt_lines_2 = {"Circle Extension": lines["Circle Extension"], "Orange": lines["Orange"]}
+
+# Test Case 3 Params
+test_name_3 = "Test case 3"
+start_mrt_3 = "Khatib"
+end_mrt_3 = "Kovan"
+mrt_lines_3 = {"Circle Extension": lines["Circle Extension"], "Orange": lines["Orange"], "Red": lines["Red"], "Purple": lines["Purple"]}
--- shortest_mrt_path/main.py
+++ shortest_mrt_path/main.py
@@ -1,11 +1,14 @@
 from app import App
 from config import *
 
+
 def main():
     app1 = App(test_name_1, start_mrt_1, end_mrt_1, mrt_lines_1)
     app2 = App(test_name_2, start_mrt_2, end_mrt_2, mrt_lines_2)
+    app3 = App(test_name_3, start_mrt_3, end_mrt_3, mrt_lines_3)
     app1.run()
     app2.run()
+    app3.run()
 
 
 if __name__ == "__main__":
